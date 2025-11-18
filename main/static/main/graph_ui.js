983d841ac2850f253dbb7072cdb2cac68d3a2b3e
Aleksei Lobanov <2401213370@stu.pku.edu.cn>
2025-11-18 12:48:24 +0800

    client: drawing edges

--- main/static/main/graph_ui.js
+++ main/static/main/graph_ui.js
@@ -2,11 +2,130 @@ import * as dataflow from "./dataflow.js";
 
 const graph_div = document.getElementById("graph_div");
 
+class Edge {
+	static counter = 0;
+	/**
+	 * @param {Port} output 
+	 * @param {Port} input 
+	 * @param {dataflow.Edge} e 
+	 */
+	constructor(input, output, e) {
+		Edge.counter += 1;
+		this.index = Edge.counter;
+		this.input = input;
+		this.output = output;
+		this.e = e;
+	}
+
+	render() {
+		if (this.edge) this.edge.remove();
+		if (this.hitbox) this.hitbox.remove();
+
+		console.log(`Edge.render(${this.index})`);
+		const edges = document.getElementById("edges_svg");
+		const container = edges.getBoundingClientRect();
+		const src = this.input.button.getBoundingClientRect();
+		const dst = this.output.button.getBoundingClientRect();
+
+		const x1 = src.left + src.width / 2 - container.left;
+		const y1 = src.top + src.height / 2 - container.top;
+		const x2 = dst.left + dst.width / 2 - container.left;
+		const y2 = dst.top + dst.height / 2 - container.top;
+
+		this.edge = document.createElementNS("http://www.w3.org/2000/svg", "line");
+		this.edge.id = `edge_${this.index}`
+		this.edge.setAttribute("x1", x1);
+		this.edge.setAttribute("y1", y1);
+		this.edge.setAttribute("x2", x2);
+		this.edge.setAttribute("y2", y2);
+		this.edge.setAttribute("stroke", "black");
+		this.edge.setAttribute("stroke-width", "2");
+
+		this.hitbox = document.createElementNS("http://www.w3.org/2000/svg", "line");
+		this.hitbox.setAttribute("x1", x1);
+		this.hitbox.setAttribute("y1", y1);
+		this.hitbox.setAttribute("x2", x2);
+		this.hitbox.setAttribute("y2", y2);
+		this.hitbox.setAttribute("stroke-width", "5");
+		this.hitbox.setAttribute("stroke", "red");
+		this.hitbox.setAttribute("stroke-opacity", "0%");
+		let is_mouseover = false;
+		this.hitbox.addEventListener("mouseover", () => {
+			this.hitbox.setAttribute("stroke-opacity", "100%");
+			this.input.button.className = "selected_port";
+			this.output.button.className = "selected_port";
+			is_mouseover = true;
+		});
+		this.hitbox.addEventListener("mouseleave", () => {
+			is_mouseover = false;
+			this.hitbox.setAttribute("stroke-opacity", "0%");
+			this.input.button.removeAttribute("class");
+			this.output.button.removeAttribute("class");
+		})
+		this.hitbox.addEventListener("click", () => {
+			if (is_mouseover) {
+				this.remove();
+				is_mouseover = false;
+				this.input.button.removeAttribute("class");
+				this.output.button.removeAttribute("class");
+			}
+		});
+
+		edges.appendChild(this.edge);
+		edges.appendChild(this.hitbox);
+	}
+
+	remove() {
+		console.debug(`remove(Edge.${this.index})`);
+
+		this.edge.remove();
+		this.hitbox.remove();
+
+		this.input.node.outs.get(this.input.name).delete(this);
+		this.output.node.ins.get(this.output.name).delete(this);
+		this.e.disconnect();
+	}
+}
+
+class Port {
+
+	/**
+	 * 
+	 * @param {Node} node 
+	 * @param {string} name 
+	 * @param {"in"|"out"} kind 
+	 */
+	constructor(node, name, kind) {
+		this.node = node;
+		this.name = name;
+		this.kind = kind;
+
+		this.button = document.createElement("button");
+		this.button.id = `port_${node.index}_${name}_${kind}`;
+		this.button.textContent = name;
+	}
+
+	/**
+	 * @returns {dataflow.Port}
+	 */
+	to_dataflow() {
+		return new dataflow.Port(this.node.n, this.name, this.kind);
+	}
+}
+
 export class Node {
+	static counter = 0;
+	static all_nodes = new Set();
+	static current_selected_port = null;
+
 	/**
 	 * @param {dataflow.Node} n 
 	 */
 	constructor(n) {
+		Node.counter += 1;
+		Node.all_nodes.add(this);
+
+		this.index = Node.counter;
 		this.n = n;
 
 		this.div = document.createElement("div");
@@ -14,12 +133,20 @@ export class Node {
 		this.x = 0;
 		this.y = 0;
 
+		/**
+		 * @type {Map<string, Set<Edge>>}
+		 */
+		this.ins = new Map();
+		/**
+		 * @type {Map<string, Set<Edge>>}
+		 */
+		this.outs = new Map();
+
+		this.div.appendChild(this.init_header());
 		this.content_div = document.createElement("div");
 		this.div.appendChild(this.content_div);
 		this.content_div.className = "node_content";
-
-		this.init_header();
-		this.init_footer();
+		this.div.appendChild(this.init_footer());
 		this.init_drag();
 
 		graph_div.appendChild(this.div);
@@ -40,11 +167,82 @@ export class Node {
 	}
 
 	init_header() {
+		const header = document.createElement("div");
+		header.className = "node_header";
+		const left_group = document.createElement("div");
+		left_group.className = "node_header_left";
+		const right_group = document.createElement("div");
+		right_group.className = "node_header_right";
+		header.appendChild(left_group);
+		header.appendChild(right_group);
+
+		left_group.appendChild(this.init_port_group("in", this.n.in_channel_names()));
+		const label = document.createElement("span");
+		label.textContent = `Node ${this.index}`;
+		left_group.appendChild(label);
+
+		return header;
 	}
 
 	init_footer() {
+		const footer = document.createElement("div");
+		footer.className = "node_footer";
+		const left_group = document.createElement("div");
+		left_group.className = "node_footer_left";
+		const right_group = document.createElement("div");
+		right_group.className = "node_footer_right";
+		footer.appendChild(left_group);
+		footer.appendChild(right_group);
+
+		right_group.appendChild(this.init_port_group("out", this.n.out_channel_names()));
+		this.size_text = document.createElement("span");
+		this.size_text.textContent = "Size: ? x ?";
+		left_group.appendChild(this.size_text);
+
+		return footer;
 	}
 
+	/**
+	 * @param {"in" | "out"} kind 
+	 * @param {Iterable<string>} names 
+	 */
+	init_port_group(kind, names) {
+		const ports_div = document.createElement("div");
+		for (const name of names) {
+			const info = new Port(this, name, kind);
+			ports_div.appendChild(info.button);
+
+			info.button.addEventListener("click", () => {
+				const other = Node.current_selected_port;
+				if (Node.current_selected_port === null) {
+					Node.current_selected_port = info;
+					info.button.className = "selected_port";
+				} else if (Node.current_selected_port.node === info.node) {
+					other.button.removeAttribute("class");
+					Node.current_selected_port = null;
+				} else if (Node.current_selected_port.kind !== info.kind) {
+					other.button.removeAttribute("class");
+					Node.current_selected_port = null;
+					if (info.kind == "in") {
+						Node.connect(other, info);
+					} else {
+						Node.connect(info, other);
+					}
+					other.button.removeAttribute("class");
+				} else {
+					// dont connect 
+				}
+			});
+
+		}
+		return ports_div;
+	}
+
+	/**
+	 *
+	 * @param {number} x 
+	 * @param {number} y 
+	 */
 	move_to(x, y) {
 		this.x = x;
 		this.y = y;
@@ -53,6 +251,40 @@ export class Node {
 	}
 
 	render() {
+		console.debug(`render(Node.${this.index})`);
 		this.div.style = `top: ${this.y}px; left: ${this.x}px;`;
+
+		for (const port of this.ins.values()) {
+			for (const edge of port.values()) {
+				edge.render();
+			}
+		}
+		for (const port of this.outs.values()) {
+			for (const edge of port.values()) {
+				edge.render();
+			}
+		}
+	}
+
+	/**
+	 * @param {Port} input 
+	 * @param {Port} output 
+	 */
+	static connect(input, output) {
+		const df_edge = dataflow.Node.connect(input.to_dataflow(), output.to_dataflow());
+		if (!df_edge) {
+			console.debug(`Could not connect ${input.node.index}@${input.name} -> ${output.node.index}@{output.name}`);
+			return;
+		}
+		const edge = new Edge(input, output, df_edge);
+
+		if (!input.node.outs.has(input.name)) input.node.outs.set(input.name, new Set());
+		input.node.outs.get(input.name).add(edge);
+
+		if (!output.node.ins.has(output.name)) output.node.ins.set(output.name, new Set());
+		output.node.ins.get(output.name).add(edge);
+
+		input.node.render();
+		output.node.render();
 	}
 }
