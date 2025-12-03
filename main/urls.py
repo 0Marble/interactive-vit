12912e0441cf92c07f89ede5466f549ce12153d5
Aleksei Lobanov <2401213370@stu.pku.edu.cn>
2025-12-03 14:31:47 +0800

    server: move node registering into a separate function

--- main/urls.py
+++ main/urls.py
@@ -16,27 +16,31 @@ urlpatterns = [
     path("load_model", views.load_model, name="load-model"),
 ] 
 
+def register_node(path):
+    file_dir, file_name = os.path.split(path)
+    name, _ = os.path.splitext(file_name)
+
+    try:
+        spec = importlib.util.spec_from_file_location(name, path)
+        if spec is None:
+            raise ImportError(f"Could not load spec from {path}")
+        mod = importlib.util.module_from_spec(spec)
+        sys.modules[name] = mod
+        spec.loader.exec_module(mod)
+
+        endpoint = f"node/{name}"
+        urlpatterns.append(path(f"{endpoint}/description", mod.description, name=f"node-{name}-description"))
+        urlpatterns.append(path(f"{endpoint}/contents", mod.contents, name=f"node-{name}-contents"))
+        urlpatterns.append(path(f"{endpoint}/compute", mod.compute, name=f"node-{name}-compute"))
+
+        logger.error("Registered node: '%s' -> %s", name, endpoint)
+    except Exception as err:
+        logger.error("Could not register node '%s': %s", path, str(err))
+
+
 nodes_dir = os.path.join(settings.BASE_DIR, "main/nodes")
 
 for file in os.listdir(nodes_dir):
-    file = os.path.join(nodes_dir, file)
-    if not os.path.isfile(file) or not file.endswith(".py"): continue
-
-
-    file_dir, file_name = os.path.split(file)
-    name, _ = os.path.splitext(file_name)
-
-    endpoint = f"node/{name}"
-    logger.error("Found node file: '%s' -> %s", name, endpoint)
-
-    logger.error(file)
-    spec = importlib.util.spec_from_file_location(name, file)
-    if spec is None:
-        raise ImportError(f"Could not load spec from {file}")
-    mod = importlib.util.module_from_spec(spec)
-    sys.modules[name] = mod
-    spec.loader.exec_module(mod)
-
-    urlpatterns.append(path(f"{endpoint}/description", mod.description, name=f"node-{name}-description"))
-    urlpatterns.append(path(f"{endpoint}/contents", mod.contents, name=f"node-{name}-contents"))
-    urlpatterns.append(path(f"{endpoint}/compute", mod.compute, name=f"node-{name}-compute"))
+    file_path = os.path.join(nodes_dir, file)
+    if not os.path.isfile(file_path) or not file_path.endswith(".py"): continue
+    register_node(file_path)
